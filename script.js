92b9e941136258788094bdc76916930c1987fc00
Juan Sebastian Morel Ruiz Diaz <juanse1990@live.com.ar>
2023-12-12 02:16:54 -0300

    Fix: contact screen fix and html meta

--- script.js
+++ script.js
@@ -31,9 +31,9 @@ window.onscroll = () => {
       sec.classList.add("show-animate");
     }
     // if want to use animation that repeats on scroll use this
-    else {
-      sec.classList.remove("show-animate");
-    }
+    // else {
+    //   sec.classList.remove("show-animate");
+    // }
   });
 
   // sticky header
